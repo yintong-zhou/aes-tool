cb95f1d5aee9960b5be008c4703b346a8abdf742
yintong-zhou <zhouyintong96@gmail.com>
2021-06-24 15:26:03 +0200

    commit

---
+++ AES Tool/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+using AES_Tool.src;
+
+namespace AES_Tool
+{
+    class Program
+    {
+        static Commands cmds = new Commands();
+        static Helper h = new Helper();
+
+        static void Main(string[] args)
+        {
+            h.TitleShow();
+
+            START:
+            Write(">>> ");
+            string input = ReadLine();
+            if (input.StartsWith("aes"))
+            {
+                string[] cmd = input.Split(' ');
+                if(cmd.Length > 1)
+                {
+                    if (cmd[1] == cmds.Encrypt())
+                    {
+                        Write("Password Key: ");
+                        string Password = ReadLine();
+                        cmds.EncryptCommand(cmd[2], Password);
+                        goto START;
+                    }
+                    else if (cmd[1] == cmds.Decrypt())
+                    {
+                        Write("Password Key: ");
+                        string Password = ReadLine();
+                        cmds.DecryptCommand(cmd[2], Password);
+                        goto START;
+                    }
+                    else
+                    {
+                        WriteLine("Command is not found, using 'help' to show the commands.");
+                        goto START;
+                    }
+                }
+                else
+                {
+                    WriteLine("Select the next command, please check with 'help'.");
+                    goto START;
+                }
+               
+            }
+            else if (input == "help")
+            {
+                cmds.HelpCommand();
+                goto START;
+            }
+            else
+            {
+                WriteLine("Start with 'aes' command or write 'help' to check the commands.");
+                goto START;
+            }
+        }
+    }
+}
---
+++ AES Tool/src/Commands.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace AES_Tool.src
+{
+    class Commands
+    {
+        CryptoAES aes = new CryptoAES();
+        string nl = Environment.NewLine;
+
+        public string Help() => "help";
+        public string Encrypt() => "-e";
+        public string Decrypt() => "-d";
+        
+        void Cmds()
+        {
+            WriteLine("Start with 'aes' command to recall other commands.");
+            WriteLine("Encrypt      aes -e [folder|file|text] {aes -e c:\\foldername, aes -e c:\\filename.txt, aes -e 'clear text'}");
+            WriteLine("Decrypt      aes -d [folder|file|text] {aes -e c:\\foldername, aes -e c:\\filename.txt, aes -e 'chyper text'}");
+        }
+
+        public void HelpCommand() => Cmds();
+
+        public void EncryptCommand(string Args, string Password)
+        {
+            if (!Args.Contains("'"))
+            {
+                if (!File.Exists(Args)) 
+                {
+                    var folder = new DirectoryInfo(Args); 
+                    if (folder.GetFiles().Length != 0)
+                    {
+                        // folder
+                    }
+                    else 
+                    {
+                        WriteLine("The directory is empty.");
+                    }
+                }
+                else
+                {
+                    WriteLine("Start to encrypting file...");
+                    aes.EncryptFile(Args, Args, Password);
+                    WriteLine($"DONE! Check in {Args}" + nl);
+                }
+            }
+            else
+            {
+                string plainText = Args.Replace("'", "").Trim();
+                string chiperText = aes.EncryptText(plainText, Password);
+                WriteLine("----------------- CHIPER TEXT BELOW-----------------");
+                WriteLine(chiperText + nl);
+            }
+        }
+
+        public void DecryptCommand(string Args, string Password)
+        {
+            if (!Args.Contains("'"))
+            {
+                if (!File.Exists(Args)) 
+                {
+                    var folder = new DirectoryInfo(Args);
+                    if (folder.GetFiles().Length != 0) 
+                    {
+                        // folder
+                    }
+                    else 
+                    {
+                        WriteLine("The directory is empty.");
+                    }
+                }
+                else
+                {
+                    WriteLine("Start to decrypting file...");
+                    aes.DecryptFile(Args, Args, Password);
+                    WriteLine($"DONE! Check in {Args}" + nl);
+                }
+            }
+            else
+            {
+                string chiperText = Args.Replace("'", "").Trim();
+                string plainText = aes.DecryptText(chiperText, Password);
+                WriteLine("----------------- PLAIN TEXT BELOW-----------------");
+                WriteLine(plainText + nl);
+            }
+        }
+    }
+}
